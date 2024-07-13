e3cfec270b44ff95db6abfbf6ef052b369b789fa
RuanDevz <Ruanbatista1509@outlook.com>
2024-07-13 02:34:42 -0300

    fix: error localstoragee

--- src/app/Hooks/UseLocalStorage.ts
+++ src/app/Hooks/UseLocalStorage.ts
@@ -1,20 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export function useLocalStorage<T>(item: string) {
-  
   const [value, setValue] = useState<T>(() => {
-    
-    const storedValue = localStorage.getItem(item);
-    return storedValue ? JSON.parse(storedValue) : [];
+    if (typeof window !== "undefined") {
+      const storedValue = localStorage.getItem(item);
+      return storedValue ? JSON.parse(storedValue) : null;
+    } else {
+      return null; // Tratamento para ambiente de servidor
+    }
   });
 
   const updateLocalStorage = (newValue: T) => {
     setValue(newValue);
-    localStorage.setItem(item, JSON.stringify(newValue));
+    if (typeof window !== "undefined") {
+      localStorage.setItem(item, JSON.stringify(newValue));
+    }
   };
+
+  // Optional: Clear localStorage item when component unmounts
+  useEffect(() => {
+    return () => {
+      if (typeof window !== "undefined") {
+        localStorage.removeItem(item);
+      }
+    };
+  }, [item]);
 
   return {
     value,
-    updateLocalStorage
+    updateLocalStorage,
   };
 }
